78c0f0fb16e20334f4b8de55c035734cf596be4c
Kevin Vera <angel.96.dark@gmail.com>
2024-11-22 16:54:45 -0600

    Crear NewEntity

---
+++ src/Empresa.Proyecto.Core/Entities/NewEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Empresa.Proyecto.Core.Entities
+{
+    /// <summary>
+    /// Entidad nueva con relacion a Simple entity a travez del campo Option
+    /// </summary>
+    public class NewEntity:BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public SimpleEntity Option { get; set; } = null!;
+    }
+}
